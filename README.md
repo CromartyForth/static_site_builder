16846b7cdb95c4c8fbdf27864c84345e6a12409b
CromartyForth <glenn@gcampbell.co.uk>
2026-03-30 17:30:19 +0100

    added textnode enum and class with __eq__ and __repr__ methods

---
+++ README.md
@@ -0,0 +1,2 @@
+Start the built in server with:
+python3 -m http.server 8888
---
+++ src/main.py
@@ -0,0 +1,9 @@
+from textnode import *
+
+print("Hello World")
+
+def main ():
+    textnode = TextNode("This is text", TextType.BOLD)
+    print(textnode)
+    return
+main()
