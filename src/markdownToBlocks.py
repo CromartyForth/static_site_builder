0eaebd25caf68e6551ea355d19e8eeff1eca7d43
CromartyForth <glenn@gcampbell.co.uk>
2026-04-01 22:38:53 +0100

    added markdownToBlocks and unit tests

--- src/markdownToBlocks.py
+++ src/markdownToBlocks.py
@@ -1,4 +1,13 @@
 
 
 def markdown_to_blocks(markdown):
-    return
+    new_blocks = []
+
+    splitList = markdown.split("\n\n")
+
+    for block in splitList:
+        if block == "":
+            continue
+        new_blocks.append(block.strip())
+    
+    return new_blocks
